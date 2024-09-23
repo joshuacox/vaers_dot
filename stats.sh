5113a9c2c3c22321dc2b85f2d5434532eafe3eaf
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2024-09-23 15:53:47 -0500

    cleanup

--- stats.sh
+++ stats.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 count_incidence () {
   echo $( cat data/${1}VAERSDATA.csv \
     |csvcut -c${2} -e'latin-1' \
@@ -19,10 +20,6 @@ for i in {1990..2024}; do
   VAX_DATE=$(count_incidence $i 19)
   ONSET_DATE=$(count_incidence $i 20)
   total=$( wc -l data/${i}VAERSDATA.csv|awk '{print $1}')
-  # perc=$(calc_perc $VAX_DATE $total)
-  # rptperc=$(calc_perc $RPT_DATE $total)
-  # rptperc=$(echo "scale=2;$RPT_DATE/$total*100" | bc)
-  # printf "%7s - VAX_DATE %7s / %7s = %7s%% RPT %7s / %7s = %7s" $i $VAX_DATE $total $perc $RPT_DATE $total $rptperc
   printf "%7s - " $i
   printerrr 'VAX' $VAX_DATE
   printerrr 'RPT' $RPT_DATE
